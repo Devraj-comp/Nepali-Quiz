dba969861347c0924b8c9790203c77553867049f
devraj <you@example.com>
2021-02-01 16:55:11 +0545

    first commit

---
+++ src/quizService/index.js
@@ -0,0 +1,220 @@
+const qBank = [
+  {
+    question:
+      "When did King Prithvi Narayan Shah died?  ",
+    answers: ["1775", "1735", "1375", "1875"],
+    correct: "1775",
+    questionId: "099099"
+  },
+  {
+    question:
+      "How long Kirant Dynasty ruled Nepal (in years)?",
+    answers: ["1000", "1222", "1118", "1881"],
+    correct: "1118",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Which one of the follweing treaty was signed at the end of Anglo Nepal War?",
+    answers: ["Kumaon-Gadhwal", "Nalapani", "Betrawati", "Sugauli"],
+    correct: "Sugauli",
+    questionId: "267908"
+  },
+  {
+    question: "Whaich 2 Nepali got VC on World War I?",
+    answers: [
+      "Kulbir Thapa and Gana Thapa",
+      "Karna Bd. Thapa and Kumar Thapa",
+      "Kulbit Thapa and Karna Bd. Thapa",
+      "Khadka Thapa and Prachanda Thapa"
+    ],
+    correct: "Kulbir Thapa and Karna Bd. Thapa",
+    questionId: "333247"
+  },
+  {
+    question: "Who built the Tudikhel of Kathmandu?",
+    answers: ["Dhir Shumsher", "Bir Shumsher", "Bhimsen Thapa", "Prithvi Narayan Shah"],
+    correct: "Bhimsen Thapa",
+    questionId: "496293"
+  },
+  {
+    question:
+      "What many Prime Ministers were there during ruling of King Mahendra?",
+    answers: [
+      "5",
+      "6",
+      "7",
+      "8"
+    ],
+    correct: "6",
+    questionId: "588909"
+  },
+  {
+    question:
+      'When did Lichhavi rule started?',
+    answers: ["200 B.C", "500 B.C", "400 B.C", "300 B.C"],
+    correct: "300 B.C",
+    questionId: "648452"
+  },
+  {
+    question: "What is the name of coin bought in use by Mandev?",
+    answers: ["Manak", "Athithi", "Pahti", "Mana"],
+    correct: "Manak",
+    questionId: "786649"
+  },
+  {
+    question:
+      "Who built Ashoka Pillar in Lumbini?",
+    answers: ["Siddhartha", "Ashoka II", "Ashoka I", "Ashoka"],
+    correct: "Ashoka",
+    questionId: "839754"
+  },
+  {
+    question:
+      "Which is the oldest temple of Nepal?",
+    answers: [
+      "Changunarayan",
+      "Pashupatinath",
+      "Swyambhunath",
+      "Boudhanath"
+    ],
+    correct: "Swyambhunath",
+    questionId: "98390"
+  },
+  {
+    question: "Who was the first Malla King?",
+    answers: ["Kumbha", "Jayasthiti", "Aridev", "Aradev"],
+    correct: "Aridev",
+    questionId: "1071006"
+  },
+  {
+    question: "Who started financial punishment for criminals?",
+    answers: ["Bhupal", "Jayasthiti", "Aridev", "Mandev"],
+    correct: "Aridev",
+    questionId: "1174154"
+  },
+  {
+    question: "Who built Rani pokhari?",
+    answers: ["Pratap Malla", "Mandev", "JayPrithvi Singh", "Junga Bd"],
+    correct: "Pratap Malla",
+    questionId: "1226535"
+  },
+  {
+    question: "When was Rani pokjari built(B.S)?",
+    answers: ["1400", "1527", "1217", "1727"],
+    correct: "1727",
+    questionId: "1310938"
+  },
+  {
+    question: "Who built Krishna temple of Patan?",
+    answers: ["Siddhi Narshimha Malla", "MohanDev Malla", "SurendraDev Malla", "DigDev Malla"],
+    correct: "Siddhi Narshimha Malla",
+    questionId: "1436873"
+  },
+  {
+    question: "Which was the capital city of Gopal Dynasty?",
+    answers: ["Judibel", "Tinkhute", "Lankhanamai", "MataTirtha"],
+    correct: "MataTirtha",
+    questionId: "1515110"
+  },
+  {
+    question: "Who was the last king of Patan?",
+    answers: ["Laxmi Narshimha", "Tej Narshimha", "Gyanendra", "Birendra"],
+    correct: "Tej Narshimha",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "Who built KasthaMandap? ",
+    answers: ["Surendra Singh", "TejaswiNarayan", "Laxmi Narayan", "Mandev"],
+    correct: "Laxmi Narayan",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "What were the armies in Mall Regime caled?",
+    answers: ["Sena", "Umrao", "Thami", "Thurke"],
+    correct: "Umrao",
+    questionId: "1822532"
+  },
+  {
+    question: "What was the original name of Gautam Buddha?",
+    answers: [
+      "George",
+      "Siddharta Gautam",
+      "Siddhartha Gauchan",
+      "Gautam Buddha"
+    ],
+    correct: "Siddhartha Gautam",
+    questionId: "195075"
+  },
+  {
+    question: 'What is the another name of Rapti zone?',
+    answers: ["Maali", "Juna", "Sworgadwari", "Angeri"],
+    correct: "Sworgadwari",
+    questionId: "2019778"
+  },
+  {
+    question: "What was the the total population of Nepal in 1911?",
+    answers: ["5.2 million", "5.5 million", "5.4 million", "5.6 million"],
+    correct: "5.6 million",
+    questionId: "2134343"
+  },
+  {
+    question: "What was the population of Nepal in 1920?",
+    answers: [
+      "5.6 million",
+      "5.5 million",
+      "6.6 million",
+      "6 million"
+    ],
+    correct: "5.5 million",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "What  is the no of ethnic groups in Nepal?",
+    answers: ["126", "136", "36", "100"],
+    correct: "126",
+    questionId: "2379831"
+  },
+  {
+    question: "What is the popular dance of Magar community Nepal?",
+    answers: ["Kaura", "Garuda", "Taruni", "Maruni"],
+    correct: "Maruni",
+    questionId: "2426418"
+  },
+  {
+    question: "At what age did King Prithvi Narayan Shah died?",
+    answers: ["52", "53", "73", "30"],
+    correct: "52",
+    questionId: "2510086"
+  },
+  {
+    question: "Who was the first king of Nepal?",
+    answers: [
+      "Mandev",
+      "Prithvi Narayan Shah",
+      "Birendra",
+      "Gyanendra"
+    ],
+    correct: "Prithvi Narayan Shah",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Which among these is a popular cultural dance of Newar community?",
+    answers: ["Gaura", "Maruni", "Kaura", "NawaDurga"],
+    correct: "NawaDurga",
+    questionId: "2796884"
+  },
+  {
+    question: "When was the Nepal curent flag adopted?",
+    answers: ["1562", "1692", "1962", "1999"],
+    correct: "1962",
+    questionId: "2838900"
+  }
+  ];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
